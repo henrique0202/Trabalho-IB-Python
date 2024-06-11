6ab24cd077ed6e32f39f77ac4692e1f2de0f8480
henrique0202 <henrique.silva101@aluno.ce.gov.br>
2024-06-11 09:34:16 -0300

    primeiro commit

---
+++ questoes/Q2.py
@@ -0,0 +1,10 @@
+numero_inteiro = 10
+numero_float = 3.14
+texto_string = "Hello world"
+valor_bool = True
+print("valor de numero_inteiro é:", numero_inteiro, "do tipo:", type(numero_inteiro))
+print("valor de numero_float é:", numero_float, "do tipo:", type(numero_float))
+print("valor de texto_string é:", texto_string, "do tipo:", type(texto_string))
+print("valor de valor_bool é:", valor_bool, "do tipo:", type(valor_bool))
+
+
